cb32f92fb43af85cce20bc59f6cd0fb45833fb32
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-18 15:29:22 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(collection)
   i = 0
   a = []
   while i < collection.length
-    yield collection[i]
-    a.push(collection[i])
+    yield a.push(collection[i])
     i += 1
   end
   return a
